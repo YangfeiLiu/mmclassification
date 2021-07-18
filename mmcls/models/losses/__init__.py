422c757e287a45aae5049b90238fbe038ee766aa
liuyangfei <33997865+YangfeiLiu@users.noreply.github.com>
2021-07-18 18:36:18 +0800

    Update __init__.py

--- mmcls/models/losses/__init__.py
+++ mmcls/models/losses/__init__.py
@@ -3,6 +3,7 @@ from .asymmetric_loss import AsymmetricLoss, asymmetric_loss
 from .cross_entropy_loss import (CrossEntropyLoss, binary_cross_entropy,
                                  cross_entropy)
 from .sce_loss import SCELoss
+from .asl import (AsymmetricLoss, AsymmetricLossOptimized, ASLSingleLabel)
 from .focal_loss import FocalLoss, sigmoid_focal_loss
 from .label_smooth_loss import LabelSmoothLoss
 from .utils import (convert_to_one_hot, reduce_loss, weight_reduce_loss,
@@ -12,5 +13,6 @@ __all__ = [
     'accuracy', 'Accuracy', 'asymmetric_loss', 'AsymmetricLoss',
     'cross_entropy', 'binary_cross_entropy', 'CrossEntropyLoss', 'reduce_loss',
     'weight_reduce_loss', 'LabelSmoothLoss', 'weighted_loss', 'FocalLoss',
-    'sigmoid_focal_loss', 'convert_to_one_hot', 'SCELoss'
+    'sigmoid_focal_loss', 'convert_to_one_hot', 'SCELoss', 'AsymmetricLoss',
+    'AsymmetricLossOptimized', 'ASLSingleLabel'
 ]
